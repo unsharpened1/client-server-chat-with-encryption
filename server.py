6eaf38bad91948d7d0553cdb7ef038daadcc2ee8
unsharpened <erroneous2the7ky@proton.me>
2026-07-02 00:53:18 +0300

    fix: prevented race condition when working with a list of clients

--- server.py
+++ server.py
@@ -7,6 +7,7 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((host, port))
 
 clients = []
+clients_lock = threading.Lock()
 
 def handler(sock):
 	buffersize = 1024
@@ -15,24 +16,28 @@ def handler(sock):
 		raw_data = sock.recv(buffersize)
 
 		if raw_data == b'':
-			for i in range(len(clients)):
-				if clients[i] == sock:
-					clients.pop(i)
+			with clients_lock:
+				for i in range(len(clients)):
+					if clients[i] == sock:
+						clients.pop(i)
 
 			sock.close()
 			return
-
-		for i in clients:
-			if i != sock:
-				i.sendall(raw_data)
-			else:
-				continue
+			
+		with clients_lock:
+			for i in clients:
+				if i != sock:
+					i.sendall(raw_data)
+				else:
+					continue
 
 s.listen()
 
 while True:
 	client_socket, client_address = s.accept()
-	clients.append(client_socket)
+
+	with clients_lock:
+		clients.append(client_socket)
 
 	t = threading.Thread(target=handler, args=(client_socket,))
 	t.start()
